f5e0f10eed1e56dfee1bbe7067e10fcbca95edd1
davySorokin <kirilldavidsorokin@gmail.com>
2024-10-06 17:43:30 +0900

    First Implementation and changes.

--- MyPlayground.playground/Pages/basics.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/basics.xcplaygroundpage/Contents.swift
@@ -2,3 +2,20 @@ import Foundation
 
 var greeting = "Hello, playground"
 print(greeting)
+
+// This is a comment.
+// More comments!
+/*
+ Multi line!
+ Styling Preference
+ */
+
+// Naming Conventions - Camel Case (always use)
+/*
+ 'let' does allow for changing - non-dynamic
+ 'var' allows for changing - dynamic
+ */
+var firstGreeting = "Hello World!"
+let firstIsMyFirstGreeting = "Hello World! #2"
+firstGreeting = "Kirill"
+print(firstGreeting)
